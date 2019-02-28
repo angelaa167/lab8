7175c7f6418cf7d39646f03380df881ec760073c
Angela <aml167@ucsd.edu>
2019-02-28 00:11:38 -0800

    map

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -2,12 +2,16 @@ function initMap() {
 	// add your code here
 	L.mapquest.key = 'IXm3G0yPjbjnzZVq4Z2ss502AZuV5fH7';
 
+	// 'map' refers to a <div> element with the ID map
 	var map = L.mapquest.map('map', {
-		center: [32.878806, -177.235912], 
+		center: [32.88, -117.236],
 		layers: L.mapquest.tileLayer('map'),
-		zoom: 12, 
-		zoomControl: false 
-	});
+		zoom: 12,
+		zoomControl: false
+	})
 
-	L.marker([32.878806, -177.235912]).addTo(map); 
-}
+	// Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
+} 
+
+
