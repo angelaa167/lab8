9054e590b14e6ee38cf877a50e22dc0159bfc700
Angela <aml167@ucsd.edu>
2019-02-27 20:04:33 -0800

    thirdparty API lab finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'IXm3G0yPjbjnzZVq4Z2ss502AZuV5fH7';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878806, -177.235912], 
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12, 
+		zoomControl: false 
+	});
+
+	L.marker([32.878806, -177.235912]).addTo(map); 
 }
